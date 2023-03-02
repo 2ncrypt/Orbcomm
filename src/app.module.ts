3db7eafcd8bce5821f044244aa28278b0fc51369
Hyunsu <me_2_day_@naver.com>
2023-03-02 13:18:36 +0900

    NestJS 기본 세팅 참고  : https://velog.io/@sujeong7474/01.-Nest.js-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EC%B4%88%EA%B8%B0-%EC%84%B8%ED%8C%85%ED%95%98%EA%B8%B0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validation } from './utils';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.production.env'
+          : process.env.NODE_ENV === 'development'
+          ? '.development.env'
+          : '.development.env',
+      isGlobal: true,
+      validationSchema: validation,
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/HttpExceptionFilter.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('SERVER_PORT');
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors(); //Cors 활성화
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(PORT);
+  if (configService.get('NODE_ENV') === 'development') {
+    Logger.log(`Application running on port ${PORT}, http://localhost:${PORT}`);
+  }
 }
+
 bootstrap();
